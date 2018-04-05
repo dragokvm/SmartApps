4251d79e9cfb49548fb0af6ee10ca00d56a3952e
dragokvm <dragokvm0@gmail.com>
2018-04-05 15:49:21 -0500

    Update cycle-switch-every-x-minutes.groovy

--- cycle-switch-every-x-minutes.groovy
+++ cycle-switch-every-x-minutes.groovy
@@ -1,5 +1,5 @@
 /**
- *  Copyright 2015 SmartThings
+ *  Copyright 2018 SmartThings
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -10,56 +10,50 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
- *  Cycle Switch every "x" minutes
- *  Choose how long the switch stays on for and how long it stays off for.
+ *  Turn on dimmer to 30% for 15 seconds then increase to 100%
  *
- *  Author: Lenny Cunningham
+ *  Author: KiMo
  */
 definition(
-    name: "Cycle Switch every 'x' minutes.",
-    namespace: "lennyz71",
-    author: "Lenny Cunningham",
-    description: "Cycle a switch on and off every 'x' minutes. Choose how long the switch stays on for and how long it stays off for.",
+    name: "No Blink Dimmer",
+    namespace: "dragokvm",
+    author: "KiMo",
+    description: "Turn on dimmer to 30% for 15 seconds then increase to 100%",
     category: "Convenience",
     iconUrl: "https://s3.amazonaws.com/smartapp-icons/Meta/light_contact-outlet.png",
     iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Meta/light_contact-outlet@2x.png"
 )
 
 preferences {
-	section("Switch to cycle every x minutes..."){
-		input "switch1", "capability.switch"
+	section("Which Dimmer?"){
+		input "switch", "capability.switch"
 	}
-    section("Automatically turn switch on for this long...") {
-        input "minutesOn", "number", title: "On time (in minutes):", required: true
-    }
-    section("Automatically turn switch off for this long...") {
-        input "minutesOff", "number", title: "Off time (in minutes):", required: true
-    }
+    	section("Automatically turn switch on for this long...") {
+		input "minutesOn", "number", title: "On time (in minutes):", required: true
+    	}
 }
 
 def installed() {
 	log.debug "Installed with settings: ${settings}"
-    initialize()
+    	initialize()
 }
 
 def updated() {
 	log.debug "Updated with settings: ${settings}"
-    initialize()
+    	initialize()
 }
 
 def initialize()
 {
-    turnOnSwitch()
+    turnOnSwitch_30()
 }
 
-def turnOnSwitch() {
-	switch1.on()
+def turnOnSwitch_30() {
+	//switch.on()
+	switch.setLevel('30')
 	def delay = (minutesOn * 60)
-	runIn(delay, turnOffSwitch)
+	runIn(delay, turnOnSwitch_100)
 }
-
-def turnOffSwitch() {
-	switch1.off()
-    def delay = (minutesOff * 60) 
-    runIn(delay, turnOnSwitch)
+def turnOnSwitch_100() {
+	switch.setLevel('100')
 }
